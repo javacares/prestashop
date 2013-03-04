a19a44e5150fef33c9cfa2798775fc202794ca5d
javacares <javacares@gmail.com>
2013-03-04 14:14:07 +0800

    初步完成配置

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop_1');
+define('_DB_USER_', 'prestashop');
+define('_DB_PASSWD_', 'prestashop');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', 'YZwCtvmq75LLqCKztAxrdWJTIaKaAxrkNTaubZSzWd1ZPBjhk4S37ENZ');
+define('_COOKIE_IV_', 'LCfJNNQk');
+define('_PS_CREATION_DATE_', '2013-03-04');
+define('_PS_VERSION_', '1.5.3.1');
+define('_RIJNDAEL_KEY_', 'wpY2bN1oCFarTsok5LrDsHv1twqDLakZ');
+define('_RIJNDAEL_IV_', 's2Bv9POq9c9FOZ0VGThVZQ==');
